e541950d5b61c61a63409bdab7f6033168180eb2
Yanuar Nugraha <yanuart.adityan@gmail.com>
2022-11-02 08:26:26 +0100

    First working skeleton docker-compose
    - Added README
    - Added .gitignore
    - Added .env
    - Removed .venv

---
+++ api/app/main.py
@@ -0,0 +1,25 @@
+# app/main.py
+from fastapi import FastAPI
+from app.db import database, User
+
+app = FastAPI(
+    title="CliniqueAPI"
+)
+
+@app.get("/")
+async def read_root():
+    return await User.objects.all()
+
+@app.on_event("startup")
+async def startup():
+    if not database.is_connected:
+        await database.connect()
+
+    # create a dummy
+    await User.objects.get_or_create(email="tester@test.com")
+
+@app.on_event("shutdown")
+async def shutdown():
+    if database.is_connected:
+        await database.disconnect()
+
