a169ad877f929a24ca017d694140ad55ad9df96f
Yanuar Tri Aditya Nugraha <yanuart.adityan@gmail.com>
2022-11-15 21:13:38 +0100

    fix alembic config and env

---
+++ api/carbon/test/test_group.py
@@ -0,0 +1,34 @@
+import asyncio
+import datetime 
+
+from carbon.db import database
+from carbon.models.group import Group
+
+
+async def create():
+    # Create some records to work with through QuerySet.create method.
+    # Note that queryset is exposed on each Model's class as objects
+
+    # method 2
+    await Group.objects.create(
+        group_name="Family-Doggonebad",
+        is_max_reached=False
+    )
+
+    await Group.objects.create(
+        group_name="Family-Catto",
+        is_max_reached=False
+    )
+
+    # to read more about inserting data into the database
+    # visit: https://collerek.github.io/ormar/queries/create/
+
+async def with_connect(function):
+    # note that for any other backend than sqlite you actually need to
+    # connect to the database to perform db operations
+    async with database:
+        await function()
+
+for func in [create]:
+    print(f"executing {func.__name__}")
+    asyncio.run(with_connect(func))
