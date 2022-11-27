84552ce94fa4b5ddf2c0e838b4c4659379f88117
Yanuar Tri Aditya Nugraha <yanuart.adityan@gmail.com>
2022-11-27 22:40:54 +0100

    added test to populate constant

--- api/carbon/test/test_populate.py
+++ api/carbon/test/test_populate.py
@@ -38,22 +38,63 @@ DUMMY_GROUP = [
     "Senior Club"
 ]
 
+DUMMY_STOP_TYPE = [
+    [0, "Reserved"],
+    [2, "Reserved"],
+    [4, "Reserved"],
+    [8, "Reserved"]
+]
+
+DUMMY_MODE_TYPE = [
+    [0, "Bus"],
+    [2, "KRL"],
+    [4, "MRT"],
+    [8, "KA"],
+]
+
+DUMMY_PLATFORM_TYPE = [
+    [0, "Roadside"],
+    [2, "Underground"],
+    [4, "Multi-Stories"],
+    [8, "Reserved"],
+    [16, "Reserved"]
+]
+
+DUMMY_STOP = [
+    ["LBS", "Lebak Bulus", 4],
+    ["FWT", "Fatmawati", 2],
+    ["BLA", "Blok A", 2],
+    ["BLM", "Blok M", 3],
+    ["SYN", "Senayan", 4],
+    ["KNG", "Kuningan", 2],
+    ["SDM", "Sudirman", 4],
+    ["THA", "Thamrin", 2],
+    ["MNS", "Monas", 4],
+    ["POI", "Pondok Indah", 2],
+    ["CDG", "Ciledug", 2],
+    ["BIN", "Bintaro", 2],
+    ["JUR", "Jurang Mangu", 2],
+    ["SDR","Sudimara", 2],
+    ["SRP", "Serpong", 2],
+    ["TAB", "Tanah Abang", 2] 
+]
+
 async def clean_all():
     await User.objects.delete(each=True)
     await Group.objects.delete(each=True)
     await Owner.objects.delete(each=True)
 
     await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
+    await Platform.objects.delete(each=True)
+    await Stint.objects.delete(each=True)
+    await ModeType.objects.delete(each=True)
+    await PlatformType.objects.delete(each=True)
+    await StopType.objects.delete(each=True)
 
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
-    await Stop.objects.delete(each=True)
+    await Bus.objects.delete(each=True)
+    await Tram.objects.delete(each=True)
+    await VehClass.objects.delete(each=True)
+    await Drive.objects.delete(each=True)
 
 
 async def populate_users():
@@ -97,12 +138,46 @@ async def populate_users():
 
 
 async def populate_stops():
-    pass
+    
+    # populate Stop
+    for stop in DUMMY_STOP:
+        await Stop.objects.create(
+            initial=stop[0],
+            name=stop[1],
+            gps_lat=random.uniform(-6.8, -5.4),
+            gps_lon=random.uniform(104.0, 110.0)
+        )
 
 async def populate_vehicles():
     pass
 
 async def populate_constant():
+    
+    # populate stop type
+    for stype in DUMMY_STOP_TYPE:
+        await StopType.objects.create(
+            id=stype[0],
+            label=stype[1],
+            description="reserved..."
+        )
+
+    # populate mode type
+    for mtype in DUMMY_MODE_TYPE:
+        await ModeType.objects.create(
+            id=mtype[0],
+            label=mtype[1],
+            description="reserved..."
+        )
+
+    # populate platform type
+    for ptype in DUMMY_PLATFORM_TYPE:
+        await PlatformType.objects.create(
+            id=ptype[0],
+            label=ptype[1],
+            description="reserved..."
+        )
+
+    # populate vehicle class
     pass
 
 async def with_connect(function):
@@ -111,7 +186,9 @@ async def with_connect(function):
 
 for func in [
     clean_all,
-    populate_users
+    populate_users,
+    populate_stops,
+    populate_constant
 ]:
     print(f"executing {func.__name__}...")
     asyncio.run(with_connect(func))
